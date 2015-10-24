b8a915b963af2f87dbdb541abdfeb21ba66426f3
equick <edwardquick@hotmail.com>
2015-10-24 21:19:34 +0000

    added aws S3 to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,14 @@ Rails.application.configure do
   
   # Added as per Devise installation instructions
   config.action_mailer.default_url_options = { host: 'http://pinterested73.herokuapp.com/' }
+  
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['S3_BUCKET_NAME'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  }
+
 end
