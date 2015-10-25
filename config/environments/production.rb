aca687a1cc151afe7ce4886bf4013f9769389e05
equick <edwardquick@hotmail.com>
2015-10-25 09:34:37 +0000

    adding fix for s3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,11 +78,12 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   
   # Added as per Devise installation instructions
-  config.action_mailer.default_url_options = { host: 'http://pinterested73.herokuapp.com/' }
+  config.action_mailer.default_url_options = { host: 'http://pinteresjted73.herokuapp.com/' }
   
   config.paperclip_defaults = {
     :storage => :s3,
-    :default_options[:url] = ':s3_domain_url',
+    :s3_host_name = 's3-eu-west-1.amazonaws.com',
+    :url = ':s3_domain_url',
     :s3_credentials => {
       :bucket => ENV['S3_BUCKET_NAME'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
