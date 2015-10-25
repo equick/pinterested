3d8124926735d3a975f0ba99ae398c8f94734cf0
equick <edwardquick@hotmail.com>
2015-10-25 09:30:46 +0000

    adding fix for s3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,6 +82,7 @@ Rails.application.configure do
   
   config.paperclip_defaults = {
     :storage => :s3,
+    :default_options[:url] = ':s3_domain_url',
     :s3_credentials => {
       :bucket => ENV['S3_BUCKET_NAME'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
